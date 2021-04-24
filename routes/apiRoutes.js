398074c60547ab18320e60066639008b2e0f2e85
Travis Springer <travis.springer@gmail.com>
2021-04-24 12:34:57 -0500

    Added DELETE functionality

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,13 +2,14 @@
 const fs = require('fs');
 
 module.exports = (app) => {
+//GET for pulling notes
     app.get('/api/notes', (req, res) => {
         fs.readFile('./db/db.json', (err, data) => {
             if(err) throw err;
             res.send(JSON.parse(data));
         })
     });
-
+//POST for adding notes
     app.post('/api/notes', (req, res) => {
         const newNote = req.body;
 
@@ -16,7 +17,7 @@ module.exports = (app) => {
             if(err) throw err;
             noteData = JSON.parse(data);
             noteData.push(newNote);
-
+            //Adding id numbers
             let num = 1
             noteData.forEach((note, index) => {
                 note.id = num;
@@ -30,4 +31,23 @@ module.exports = (app) => {
         });
         res.send('Note received!');
     })
+    
+//DELETE for removing notes
+    app.delete('/api/notes/:id', (req, res) => {
+        const deleteNoteID = req.params.id;
+
+        fs.readFile('./db/db.json', (err, data) => {
+            if(err) throw err;
+            noteData = JSON.parse(data);
+            for (let i=0; i < noteData.length; i++) {
+                if (noteData[i].id === Number(deleteNoteID)) {
+                    noteData.splice([i], 1);
+                }
+            }
+            fs.writeFile('./db/db.json', JSON.stringify(noteData), (err, data) => {
+                if(err) throw err;
+            });
+        });
+        res.status(204).send();
+    });
 }
